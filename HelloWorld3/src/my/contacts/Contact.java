3e61f8b5a1a797836c2fe1bb289e5626c7000e3a
Markus Vogel <markus.vogel@live.com>
2019-03-03 21:52:37 +0100

    comitted and staged

--- HelloWorld3/src/my/contacts/Contact.java
+++ HelloWorld3/src/my/contacts/Contact.java
@@ -6,7 +6,7 @@ public class Contact {
 	private String phoneNumber;
 	private String emailAdress;
 	private String mobileNumber;//restereste
-	private String kategorie; //test
+	private String kategorie; //testtest
 
 	public String getMobileNumber() {
 		return mobileNumber;
